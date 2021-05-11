6b0026f9291c8aac7a816884f9e27a98bacfd6ca
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-05-11 09:31:08 +0200

    update

--- wsl.sh
+++ wsl.sh
@@ -56,6 +56,12 @@ sudo tee /etc/fonts/local.conf > /dev/null <<EOF
 </fontconfig>
 EOF
 
+log "Installing win32yank.exe"
+curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe ~/bin
+
 log "Installing neovim plugins"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
